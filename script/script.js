e7d06dcbdf5ccf60e430a4bd4301c235fdff9b50
DonkeYSpotteD <alienjiad@gmail.com>
2026-02-23 00:37:06 +0800

    update javascript

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+
 let rejectedList = [];
 let acceptedList = [];
 
@@ -33,6 +34,7 @@ const rejectedBtn = getId("rejected-filter-btn");
 const sectionForAccepted =getId("sectionForAccepted");
 const sectionForRejected = getId("sectionForRejected");
 
+
 function toggleStyle (id){
 
     allBtn.classList.remove('btn', 'btn-info');    
@@ -46,6 +48,10 @@ function toggleStyle (id){
     const selected = document.getElementById(id) ;
     selected.classList.remove('btn','btn-soft');
     selected.classList.add('btn','btn-info');
+    if(id == 'accepted-filter-btn'){
+        allCards.classList.add('hidden')
+        sectionForAccepted.classList.remove('hidden')
+    }
     
 }
  
@@ -65,13 +71,16 @@ mainContainer.addEventListener ('click', function (event){
                         jobAbout
     }    
    const jobExist = acceptedList.find(item=> item.companyName == cardInfo.companyName)
+    decision.querySelector(".liveStatus").innerText= "Interviewed"
+    decision.querySelector(".liveStatus").className = " btn btn-soft text-[green] border-green-600 "
    if (!jobExist)
    {
     acceptedList.push(cardInfo);
    }
    
-   lengthCheck ();
+    lengthCheck ();
    
+    renderAccepted ()
    }
    
 })
@@ -79,6 +88,38 @@ mainContainer.addEventListener ('click', function (event){
 function renderAccepted ()
 {
     sectionForAccepted.innerHTML = '';
+
+    for( let accepted of acceptedList)
+    {
+        
+        let div = document.createElement("div")
+    div.className= "bg-white rounded-md p-9 space-y-4 shadow-md  transition-all duration-300 ease-in-out  hover:bg-blue-100 hover:-translate-y-1 hover:shadow-xl "
+    div.innerHTML = `<div class="flex justify-between items-center space-y-4">
+                    <div class="space-y-2">
+                        <h1 id="companyName" class="text-[#002C5C] text-[15px] font-bold">Mobile First Corp</h1>
+                        <p id="neededSkill" class="text-[grey]">React Native Developer</p>
+                    </div>
+                    <button class="cursor-pointer btn btn-error"><i class="fa-regular fa-trash-can"></i></button>
+                </div>
+
+                <p id="salaryData" class="text-[grey] text-sm"> Remote
+                    •
+                    Full-time
+                    •
+                    $130,000 - $175,000</p>
+                <button class="btn btn-soft bg-[#EEF4FF] text-[#002C5C] border-none shadow-md"> Not Applied</button>
+                <p id="jobAbout" class="text-[grey] text-sm">Build cross-platform mobile applications using React
+                    Native. Work on
+                    products used by millions of users worldwide.</p>
+                <div class="space-x-2">
+                    <button class="btn btn-soft text-[green] border-green-600 bg-white">Interview</button>
+                    <button class="btn btn-soft text-[red] bg-white border-red-600">Rejected</button>
+                </div>
+
+    ` 
+        sectionForAccepted.append(div);
+    
+    }
 }
 
 
